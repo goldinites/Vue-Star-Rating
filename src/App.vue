bf8d27fbc32245661d69293dfee710e091a2bba8
Дмитрий Бабич <babich@logema.org>
2024-03-17 16:56:05 +0400

    init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="app">
+    <div>Current Rating: {{ currentRating }}</div>
+    <StarRating
+        :step="0.1"
+        :rating="currentRating"
+        @update-rating="currentRating = $event"
+    />
+  </div>
+</template>
+
+<script setup>
+import StarRating from "@/components/StarRating.vue";
+import {ref} from "vue";
+const currentRating = ref(0)
+</script>
+
+<style scoped>
+/* this block can be deleted  */
+.app {
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  font-size: 25px;
+  line-height: 30px;
+  font-family: 'JetBrains Mono', monospace;
+  background-color: #f7f7f7;
+}
+/* this block can be deleted  */
+</style>
---
+++ src/components/StarRating.vue
@@ -0,0 +1,245 @@
+<template>
+  <div
+      ref="rating"
+      class="star-rating"
+      :style="`--stars-between: ${starsBetween}px; --star-size: ${starSize}px;`"
+      @mouseenter="enterRatingHandler"
+      @mousemove="moveRatingHandler"
+      @mouseleave="leaveRatingHandler"
+      @click="setRatingHandler"
+  >
+    <div class="star-rating__items">
+      <div
+          v-for="index in starLimit"
+          :key="index"
+          ref="star"
+          class="star"
+      >
+        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <g clip-path="url(#clip0_2697_67067)">
+            <path
+                d="M3.61163 15.443C3.22563 15.641 2.78763 15.294 2.86563 14.851L3.69563 10.121L0.17263 6.76501C-0.15637 6.45101 0.01463 5.87701 0.45563 5.81501L5.35363 5.11901L7.53763 0.792012C7.73463 0.402012 8.26763 0.402012 8.46463 0.792012L10.6486 5.11901L15.5466 5.81501C15.9876 5.87701 16.1586 6.45101 15.8286 6.76501L12.3066 10.121L13.1366 14.851C13.2146 15.294 12.7766 15.641 12.3906 15.443L7.99963 13.187L3.61063 15.443H3.61163Z"/>
+          </g>
+          <defs>
+            <clipPath id="clip0_2697_67067">
+              <rect width="16" height="16" fill="white"/>
+            </clipPath>
+          </defs>
+        </svg>
+
+      </div>
+    </div>
+    <div
+        v-if="showCurrentValue"
+        class="star-rating__value"
+        :style="`width: ${valueRatingWidth}px`"
+    >
+      <div
+          v-for="index in starLimit"
+          :key="index"
+          class="star filled"
+      >
+        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <g clip-path="url(#clip0_2697_67067)">
+            <path
+                d="M3.61163 15.443C3.22563 15.641 2.78763 15.294 2.86563 14.851L3.69563 10.121L0.17263 6.76501C-0.15637 6.45101 0.01463 5.87701 0.45563 5.81501L5.35363 5.11901L7.53763 0.792012C7.73463 0.402012 8.26763 0.402012 8.46463 0.792012L10.6486 5.11901L15.5466 5.81501C15.9876 5.87701 16.1586 6.45101 15.8286 6.76501L12.3066 10.121L13.1366 14.851C13.2146 15.294 12.7766 15.641 12.3906 15.443L7.99963 13.187L3.61063 15.443H3.61163Z"/>
+          </g>
+          <defs>
+            <clipPath id="clip0_2697_67067">
+              <rect width="16" height="16" fill="white"/>
+            </clipPath>
+          </defs>
+        </svg>
+
+      </div>
+    </div>
+    <div
+        v-if="!showCurrentValue"
+        class="star-rating__plug"
+        :style="`width: ${plugRatingWidth}px`"
+    >
+      <div
+          v-for="index in starLimit"
+          :key="index"
+          class="star filled"
+      >
+        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <g clip-path="url(#clip0_2697_67067)">
+            <path
+                d="M3.61163 15.443C3.22563 15.641 2.78763 15.294 2.86563 14.851L3.69563 10.121L0.17263 6.76501C-0.15637 6.45101 0.01463 5.87701 0.45563 5.81501L5.35363 5.11901L7.53763 0.792012C7.73463 0.402012 8.26763 0.402012 8.46463 0.792012L10.6486 5.11901L15.5466 5.81501C15.9876 5.87701 16.1586 6.45101 15.8286 6.76501L12.3066 10.121L13.1366 14.851C13.2146 15.294 12.7766 15.641 12.3906 15.443L7.99963 13.187L3.61063 15.443H3.61163Z"/>
+          </g>
+          <defs>
+            <clipPath id="clip0_2697_67067">
+              <rect width="16" height="16" fill="white"/>
+            </clipPath>
+          </defs>
+        </svg>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StarRating',
+  props: {
+    rating: {
+      type: [Number, String],
+      default: 0,
+    },
+    starLimit: {
+      type: Number,
+      default: 5,
+    },
+    readOnly: {
+      type: Boolean,
+      default: false,
+    },
+    step: {
+      type: Number,
+      default: 1,
+    },
+    starSize: {
+      type: Number,
+      default: 48,
+    },
+    starsBetween: {
+      type: Number,
+      default: 8
+    }
+  },
+  data() {
+    return {
+      currentPos: 0,
+      containerWidth: 0,
+      containerLeft: 0,
+      currentRating: 0,
+      plugRatingWidth: 0,
+      valueRatingWidth: 0,
+      showCurrentValue: true
+    };
+  },
+  computed: {
+    maxSteps() {
+      return this.starLimit / this.step;
+    },
+    stepSize() {
+      return this.containerWidth / this.maxSteps;
+    },
+    ratingDecimalLength() {
+      return this?.step?.toString()?.split('.')?.[1]?.length ?? 0;
+    },
+    lastRatingValue() {
+      return Math.ceil(this.currentPos / this.stepSize) * this.step;
+    },
+    preparedRatingValue() {
+      return +this.lastRatingValue?.toFixed(this?.ratingDecimalLength) ?? 0;
+    }
+  },
+  created() {
+    if (this.rating) {
+      this.currentRating = +this.rating ?? 0;
+    }
+  },
+  mounted() {
+    if (typeof window !== 'undefined') {
+      const containerSizes = this.$refs?.rating?.getBoundingClientRect();
+      this.containerWidth = containerSizes?.width ?? 0;
+      this.containerLeft = containerSizes?.left ?? 0;
+
+      if (this.currentRating) {
+        this.setValueRatingWidth();
+      }
+    }
+  },
+  methods: {
+    enterRatingHandler() {
+      if (!this.readOnly) {
+        this.showCurrentValue = false;
+      }
+    },
+    leaveRatingHandler() {
+      if (!this.readOnly) {
+        this.showCurrentValue = true;
+      }
+    },
+    moveRatingHandler(e) {
+      if (!this.readOnly) {
+        this.currentPos = e.pageX - this.containerLeft;
+        this.setPlugRatingWidth();
+
+        this.$emit('move-rating', this.preparedRatingValue);
+      }
+    },
+    setRatingHandler() {
+      if (!this.readOnly) {
+        this.currentRating = this.preparedRatingValue;
+
+        this.setValueRatingWidth();
+        this.$emit('update-rating', this.currentRating);
+      }
+    },
+    setPlugRatingWidth() {
+      this.plugRatingWidth = this.calculateValueRatingWidth(this.lastRatingValue);
+    },
+    setValueRatingWidth() {
+      this.valueRatingWidth = this.calculateValueRatingWidth(this.currentRating);
+    },
+    calculateValueRatingWidth(value) {
+      const decimal = +`0.${value?.toString()?.split('.')?.[1] ?? 0}`;
+      const currentStarIndex = Math.ceil(value - 1);
+      const currentStarElement = this.$refs?.star?.[currentStarIndex];
+
+      if (currentStarElement) {
+        const currentStarPosition = currentStarElement?.getBoundingClientRect();
+        const starFillWidth = decimal ? (this.starSize * decimal) : this.starSize;
+
+        return ((currentStarPosition?.left - this.containerLeft) + starFillWidth) ?? 0;
+      }
+
+      return 0;
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+
+.star {
+  width: var(--star-size);
+  height: var(--star-size);
+  flex-shrink: 0;
+
+  svg {
+    width: 100%;
+    height: 100%;
+    fill: #F3BB93;
+  }
+
+  &.filled {
+    svg {
+      fill: #ff6600;
+    }
+  }
+
+  &-rating {
+    position: relative;
+
+    &__items {
+      display: flex;
+      gap: var(--stars-between);
+    }
+
+    &__plug, &__value {
+      position: absolute;
+      display: flex;
+      gap: var(--stars-between);
+      top: 0;
+      left: 0;
+      width: 0;
+      overflow: hidden;
+    }
+  }
+}
+</style>
